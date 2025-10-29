b7ced0c136019c9c865286c6ee4cfdc57572b8bd
Ale3320 <alejandro.parada2307@gmail.com>
2025-10-29 16:41:01 -0500

    Cuerpo de campaignModel
    Se agrega el cuerpo al modelo de campania para revision.

--- src/models/campaignModel.js
+++ src/models/campaignModel.js
@@ -1,6 +1,28 @@
 const mongoose = require("mongoose"); // importando el componente mogoose
 const campaignSchema = mongoose.Schema({
-
-
+    campaignName: {
+        type: String,
+        required: true
+    },
+    crowdfounder: {
+        type: String,
+        required: true
+    },
+    crowdfounderNIT: {
+        type: String,
+        required: true
+    },
+    documents: {
+        type: String,
+        required: true
+    },
+    campaignObjectives: {
+        type: String,
+        required: true
+    },
+    serviceOrProduct: {
+        type: String,
+        required: true
+    },
 });
 module.exports = mongoose.model("Campaign", campaignSchema);
