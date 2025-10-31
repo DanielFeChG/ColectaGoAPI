173428d9e0f0e8170b04d033e8d24465ff47bdf5
Ale3320 <alejandro.parada2307@gmail.com>
2025-10-30 23:37:04 -0500

    Ruta campaign.js a index.js
    Se agrega la ruta de campaign.js al index.js y se procede a realizar la prueba en Postman

--- src/index.js
+++ src/index.js
@@ -3,13 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const authRoutes = require("./routes/authentication.js");
+const campaignRoutes = require("./routes/campaign.js");
+
 const mongoose = require("mongoose");
 require('dotenv').config();
 app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
 app.use(parser.json()); // transforma los datos a formato JSON
+
 //Gestión de las rutas usando el middleware
 app.use("/api", authRoutes);
+app.use("/api", campaignRoutes);
+
 app.use(express.json());
+
 //Conexión a la base de datos
 mongoose
     .connect(process.env.MONGODB_URI)
